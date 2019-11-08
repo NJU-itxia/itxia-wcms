69e1f7dcc3b109198f33f8b9189890c64e99f274
Zhenxi <PiscesXP@outlook.com>
2019-11-08 18:43:10 +0800

    添加默认路由

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { view as Login } from "./components/login";
 import { view as MainPage } from "./components/mainPage";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <Router>
+      <Route exact={true} path="/">
+        <Redirect to="/login"></Redirect>
+      </Route>
       <Route path="/login">
         <Login></Login>
       </Route>
       <Route path="/home">
         <MainPage></MainPage>
+      </Route>
+      <Route path="*">
+        <span>Page not found.</span>
       </Route>
     </Router>
   );
