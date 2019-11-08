00c29ced036b85ec7a6d49a73111c36041f8a4d6
Zhenxi <PiscesXP@outlook.com>
2019-11-08 20:24:32 +0800

    完成selfInfo

--- src/components/selfInfo/view.jsx
+++ src/components/selfInfo/view.jsx
@@ -9,9 +9,6 @@ import {
   Alert
 } from "antd";
 import React from "react";
-import { connect } from "react-redux";
-import * as actions from "./actions";
-import requestStatus from "../../util/requestStatus";
 import * as api from "../../util/api";
 
 const { Option } = Select;
@@ -35,31 +32,66 @@ class SelfInfo extends React.Component {
       }
     };
     this.fetchSelfInfo = this.fetchSelfInfo.bind(this);
-  }
-
-  componentDidUpdate() {
-    const { status, updateError } = this.props.update;
-    let isNotificated = true;
-    if (status === requestStatus.SUCC) {
-      notification.success({ message: "更新个人信息成功" });
-    } else if (status === requestStatus.ERROR) {
-      notification.error({
-        message: "更新个人信息失败",
-        description: String(updateError),
-        duration: 0
-      });
-    } else {
-      isNotificated = false;
-    }
-    if (isNotificated) {
-      this.props.onNotification();
-    }
+    this.handleUpdateSelfInfo = this.handleUpdateSelfInfo.bind(this);
   }
 
   componentDidMount() {
     this.fetchSelfInfo();
   }
 
+  fetchSelfInfo() {
+    this.setState({
+      selfInfo: {
+        isLoad: false
+      }
+    });
+    api
+      .post("/admin/info", "")
+      .then(res => {
+        if (res.data.success === true) {
+          this.setState({
+            selfInfo: {
+              isLoad: true,
+              payload: res.data.data
+            }
+          });
+        } else {
+          //TODO
+        }
+      })
+      .catch(() => {
+        //TODO onReject
+      });
+  }
+
+  /**
+   * 请求API更新个人信息.
+   */
+  handleUpdateSelfInfo(info) {
+    api.post("/admin/updateInfo", info).then(res => {
+      //TODO
+      if (res.data.success === true) {
+        notification.success({ message: "ok!" });
+      }
+      this.fetchSelfInfo(); //从服务器更新信息
+    });
+  }
+
+  render() {
+    if (this.state.selfInfo.isLoad !== true) {
+      //TODO 居中转圈圈
+      return <Spin></Spin>;
+    }
+    return (
+      <WrappedSelfInfoForm
+        selfInfoPayload={this.state.selfInfo.payload}
+        onUpdateInfo={this.handleUpdateSelfInfo}
+      ></WrappedSelfInfoForm>
+    );
+  }
+}
+
+class SelfInfoForm extends React.Component {
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -72,36 +104,20 @@ class SelfInfo extends React.Component {
     });
   };
 
-  fetchSelfInfo() {
-    api.post("/admin/info", "").then(res => {
-      console.log(res.data);
-      this.setState({
-        selfInfo: {
-          isLoad: true,
-          payload: res.data.data
-        }
-      });
-    }).catch(()=>{
-      //TODO onReject
-    });
-  }
-
   render() {
     const { getFieldDecorator } = this.props.form;
     const formItemLayout = {
       labelCol: { span: 6 },
       wrapperCol: { span: 14 }
     };
-    const isLoad = !this.props.loading && !this.props.error && this.props.data;
-    return !isLoad ? (
-      <Spin></Spin>
-    ) : (
+    const payload = this.props.selfInfoPayload;
+    return (
       <Form {...formItemLayout} onSubmit={this.handleSubmit}>
         <Form.Item label="姓名">
-          <span className="ant-form-text">{String(this.props.data.name)}</span>
+          <span className="ant-form-text">{String(payload.name)}</span>
         </Form.Item>
         <Form.Item label="账户ID">
-          <span className="ant-form-text">{String(this.props.data.id)}</span>
+          <span className="ant-form-text">{String(payload.id)}</span>
         </Form.Item>
         <Form.Item label="校区" hasFeedback>
           {getFieldDecorator("location", {
@@ -115,7 +131,8 @@ class SelfInfo extends React.Component {
         </Form.Item>
         <Form.Item label="预约单邮件提醒">
           {getFieldDecorator("acceptEmail", { valuePropName: "value" })(
-            <Switch defaultChecked={this.props.data.acceptEmail} />
+            //TODO
+            <Switch defaultChecked={payload.acceptEmail} />
           )}
         </Form.Item>
 
@@ -153,7 +170,7 @@ class SelfInfo extends React.Component {
         <Form.Item wrapperCol={{ span: 12, offset: 9 }}>
           <Button
             type="primary"
-            loading={this.props.update.status === requestStatus.PENDING}
+            loading={false} //TODO
             htmlType="submit"
           >
             更新个人信息
@@ -164,21 +181,16 @@ class SelfInfo extends React.Component {
   }
 }
 
-
-
-const WrappedSelfInfo = Form.create({
+const WrappedSelfInfoForm = Form.create({
   mapPropsToFields: props => {
-    if (props.data && !props.error) {
-      const { locationRawValue, email, acceptEmail } = props.data;
-      return {
-        location: Form.createFormField({ value: locationRawValue }),
-        acceptEmail: Form.createFormField({ value: acceptEmail }), //??? 不生效
-        email: Form.createFormField({ value: email })
-      };
-    }
-    return {};
+    const { locationRawValue, email, acceptEmail } = props.selfInfoPayload;
+    return {
+      location: Form.createFormField({ value: locationRawValue }),
+      acceptEmail: Form.createFormField({ value: acceptEmail }), //??? 不生效
+      email: Form.createFormField({ value: email })
+    };
   },
   name: "validate_other"
-})(SelfInfo);
+})(SelfInfoForm);
 
-export default WrappedSelfInfo;
+export default SelfInfo;
