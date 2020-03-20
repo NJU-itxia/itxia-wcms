cefbe9d074938d8fb742ccf8c2bff76ad22a50e0
Zhenxi <PiscesXP@outlook.com>
2020-03-20 15:21:47 +0800

    移除无用旧代码

--- src/enums/actions.js
+++
@@ -1,7 +0,0 @@
-export default Object.freeze({
-  ACCEPT: { actionName: "接单", actionEnum: 0 },
-  PUT_BACK: { actionName: "放回", actionEnum: 1 },
-  COMPLETE: { actionName: "完成", actionEnum: 2 },
-  CANCEL: { actionName: "取消", actionEnum: 3 },
-  ABANDON: { actionName: "废弃", actionEnum: 4 }
-});
