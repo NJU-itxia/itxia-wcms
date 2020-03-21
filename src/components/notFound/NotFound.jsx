8e816f1e3c43a3ff2f353c8d2b12086fc6261e77
Zhenxi <PiscesXP@outlook.com>
2020-03-21 12:18:01 +0800

    增加找不到预约单的提示

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,25 +1,41 @@
 import { Result, Button } from "antd";
 import React from "react";
-import { useHistory } from "react-router-dom";
+import PropTypes from "prop-types";
 
-function NotFound() {
-  const history = useHistory();
+function NotFound(props) {
   function onBack() {
-    history.goBack();
+    window.history.back();
   }
+
+  const {
+    status = "good luck",
+    title = "Not Found",
+    subTitle = "可能还在开发中,或是出错了.",
+    icon = <img src="/img/emoji-han.png" alt="404" />,
+    extra = (
+      <Button type="primary" onClick={onBack}>
+        返回
+      </Button>
+    )
+  } = props;
+
   return (
     <Result
-      status="good luck"
-      title="Not Found"
-      subTitle="可能还在开发中,或是出错了."
-      icon={<img src="/img/emoji-han.png" alt="404" />}
-      extra={
-        <Button type="primary" onClick={onBack}>
-          返回
-        </Button>
-      }
+      status={status}
+      title={title}
+      subTitle={subTitle}
+      icon={icon}
+      extra={extra}
     />
   );
 }
+
+NotFound.propTypes = {
+  status: PropTypes.string,
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
+  icon: PropTypes.node,
+  extra: PropTypes.node
+};
 
 export { NotFound };
